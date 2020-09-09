66b1293f77ab7b42ff44ab3caa95e5fddd5ed07d
Calla Lu <46143943+clu2001@users.noreply.github.com>
2020-09-08 20:14:17 -0400

    pepe

---
+++ Endless Runner/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+    public GameObject effect;
+    private Shake shake;
+
+    public GameObject explosionSound; 
+
+    private void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<Shake>(); 
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+
+            Instantiate(explosionSound, transform.position, Quaternion.identity); 
+
+            shake.CamShake();
+
+            Instantiate(effect, transform.position, Quaternion.identity); 
+
+            // player takes damage
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+
+            Destroy(gameObject); 
+        }
+    }
+
+}
---
+++ Endless Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float YIncrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public int health = 3;
+ 
+    public Text healthDisplay;
+
+        
+    // Update is called once per frame
+    private void Update()
+
+    {
+
+        healthDisplay.text = health.ToString(); 
+
+        // if player has no more health
+        if (health <= 0)
+        {
+            SceneManager.LoadScene("GameOver"); 
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        // if player clicks up key 
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight) 
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + YIncrement);
+        }
+        // if player clicks down key 
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)   
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - YIncrement);
+        }
+    }
+}
